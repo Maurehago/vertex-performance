bf411b46f216173313e759e39d2a3868c539862a
LinuxUserGD <no-ads@mail.de>
2022-02-16 22:18:35 +0100

    Demo auf Godot 4.0 alpha aktualisiert

--- project/skript/Player.gd
+++ project/skript/Player.gd
@@ -1,23 +1,15 @@
-extends RigidBody3D
+extends RigidDynamicBody3D
 var move : Vector3 = Vector3(0,0,0)
 var rotate: Vector3 = Vector3(0,0,0)
 var camera_rotate: Vector3 = Vector3(0,0,0)
-var gravity = 0.66743*1.2
+var gravity : float = 0.66743*1.2
 var gravity_force : Vector3 = Vector3(0,-1,0)
-var ball_mass = 2
-var purse_count = 0
+var ball_mass : int = 2
+var purse_count : int = 0
 func _ready():
 	set_physics_process(true);
-#func _integrate_forces(state):
-#	var dt : float = state.get_step()
-#	state.angular_velocity = move * 100
-#	if Input.is_action_pressed("ui_select"):
-#		if get_colliding_bodies().size() != 0:
-#			state.add_central_force(-120000*2*gravity_force*dt)
-
 func _physics_process(_delta):
 	get_parent().get_node("cambase").position = self.position
-	
 	if Input.is_action_pressed("view_up"):
 		if camera_rotate.x <= deg2rad(90):
 			camera_rotate.x = camera_rotate.x+0.02
@@ -40,13 +32,11 @@ func _physics_process(_delta):
 	elif Input.is_action_pressed("move_left"):
 		if move.z > -2:
 			move.z = move.z+0.04
-	if Input.is_action_pressed("move_jump"):
-		if get_colliding_bodies().size() != 0:
-			add_central_force(300*Vector3(0,1,0))
+	if Input.is_action_pressed("move_jump") and get_colliding_bodies().size() != 0:
+			add_constant_central_force(150*Vector3(0,1,0))
+	else:
+		constant_force = Vector3(0,0,0)
 	move *= 0.95
 	angular_velocity = move.rotated(Vector3(0,1,0), rotate.y)*32
 	get_parent().get_node("cambase").rotation = rotate
 	get_parent().get_node("cambase/Camera3D").rotation = camera_rotate
-
-
-
